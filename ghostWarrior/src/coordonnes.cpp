d992dc929da8db3caa7de040ce5402aea7f7ed51
ju <ju@justine>
2016-11-23 16:38:40 +0100

    classe coordonne modif

--- ghostWarrior/src/coordonnes.cpp
+++ ghostWarrior/src/coordonnes.cpp
@@ -3,13 +3,18 @@
  *
  *  Created on: 23 nov. 2016
  *      Author: Sergio
+ *
+ *      @resume: Indication des coordonnées de l'ensemble des objets du programme
  */
 
 #include "coordonnes.h"
 
-coordonnes::coordonnes() {
-	// TODO Auto-generated constructor stub
-
+/* Constructeur des coordonnées des objets*/
+coordonnes::coordonnes(int x, int y, int lo, int la) {
+			_x = x;
+			_y = y;
+			_longueur = lo;
+			_largeur = la;
 }
 
 coordonnes::~coordonnes() {
--- ghostWarrior/src/coordonnes.h
+++ ghostWarrior/src/coordonnes.h
@@ -3,15 +3,27 @@
  *
  *  Created on: 23 nov. 2016
  *      Author: Sergio
+ *
+ *      @resume: Indication des coordonnées de l'ensemble des objets du programme
  */
-//hello hello
-// bonjour
+
 #ifndef COORDONNES_H_
 #define COORDONNES_H_
-/* hola que tale*/
+
 class coordonnes {
 public:
-	coordonnes();
+
+	/*Déclaration des attributs*/
+	private int _x;								//Point de coordonnée (x,y)
+	private int _y;
+	private int _longueur;						//Taille de l'objet
+	private int _largeur;
+
+	/* Constructeur des coordonnées des objets*/
+	coordonnes(int x, int y, int lo, int la);
+
+
+	/*Déclarations fonctions*/
 	virtual ~coordonnes();
 };
 
