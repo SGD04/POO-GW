8496813807d44d61cc02b758704d3dbfa268f6fc
jsntju <jsnt.denis@gmail.com>
2016-11-24 16:35:24 +0100

    Update MainTest.cpp

--- ghostWarrior/src/MainTest.cpp
+++ ghostWarrior/src/MainTest.cpp
@@ -5,7 +5,7 @@
  * Case 925 -163, avenue de Luminy
  * 13288 Marseille CEDEX 9
  *
- * Ce fichier est l'oeuvre d'eleves de Polytech Marseille. Il ne peut être
+ * Ce fichier est l'oeuvre d'eleves de Polytech Marseille. Il ne peut Ãªtre
  * reproduit, utilise ou modifie sans l'avis express de ses auteurs.
 */
 
@@ -20,18 +20,17 @@
 */
 
 
-#include "MainTest.h"
 #include "Coordonne.h"
 #include <iostream>
 using namespace std;
 
 
 
-int MainTest::main(void)
+int main()
 {
 	Coordonne coord(0,0,5,10);
-	cout << "Les coord sont !!" << endl;
 	coord.affichage();
 
-	return(0);
+	return 0;
 }
+
