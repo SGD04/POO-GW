0a9c0ac698261bbc83c68f0ddbff88288499587d
jsntju <jsnt.denis@gmail.com>
2016-11-25 13:46:29 +0100

    Create Joueur.cpp

---
+++ ghostWarrior/src/Joueur.cpp
@@ -0,0 +1,45 @@
+/*
+ * joueur.cpp
+ *
+ * Polytech Marseille
+ * Case 925 -163, avenue de Luminy
+ * 13288 Marseille CEDEX 9
+ *
+ * Ce fichier est l'oeuvre d'eleves de Polytech Marseille. Il ne peut être
+ * reproduit, utilise ou modifie sans l'avis express de ses auteurs.
+ */
+
+/**
+ * @author DENIS Justine <justine.denis@etu.univ-amu.fr>
+ * @author GALAN-DELEA Sergio <sergio.galan-delea@etu.univ-amu.fr>
+ *
+ * @version 1 / 25-11-2016
+ * @Resume: Classe des joueurs: personnages jouables par les utilisateurs
+ * @toto NA
+ * @bug NA
+*/
+#include "Joueur.h"
+
+/* Constructeurs des personnages:
+* @arg: nom donné par le joueur pour la partie
+* @arg: n numero du joueur dans le jeu n = {1,2} car il n'y a que deux joueurs
+* @arg: x abcisse des objets
+* @arg: y ordonnee des objets
+* @arg: lo la longueur de l'objet graphique
+* @arg: lo la longueur de l'objet graphique
+*/
+Joueur::Joueur(string nom, int n, int x, int y, int lo, int la) :  Personnage(n,x,y,lo,la)
+{
+    _nom = nom;
+}
+
+void Joueur::affiche() const
+{
+    cout<<"Joueur"<<endl;
+	cout<<"num : "<<_num<<" "<<_x<<" "<<_y<<" "<<_longueur<<" "<<_largeur<<endl;
+}
+
+Joueur::~Joueur()
+{
+    //dtor
+}
