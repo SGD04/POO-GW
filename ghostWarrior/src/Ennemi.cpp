03deb85f0c377593893dee9d656fd6bf0f81d9d7
jsntju <jsnt.denis@gmail.com>
2016-11-30 08:43:26 +0100

    Update Ennemi.cpp

--- ghostWarrior/src/Ennemi.cpp
+++ ghostWarrior/src/Ennemi.cpp
@@ -21,6 +21,7 @@
 
 #include "Ennemi.h"
 
+
 /* Constructeurs des personnages:
 * @arg: n numero du personnage dans le jeu
 * @arg: x abcisse des objets
@@ -32,6 +33,76 @@ Ennemi::Ennemi(int n, int x, int y, int lo, int la) :  Personnage(n,x,y,lo,la)
 {
     //ctor
 }
+
+/*Valeur aléatoire donne entre a et b (a<b)
+@param: a entier inclus
+@param: b entier exclus de l'intervalle
+@return: valeur comprise entre [a,b[
+*/
+int rand_a_b(int a, int b)
+{
+    return rand()%(b-a) +a;
+}
+
+/* Choix du comportement de l'ennemi (defense, attaque ou deplacement) en fonction du joueur
+* @param : Le joueur du jeu (personnage controlé par l'humain)
+*/
+void Ennemi::combattre(Joueur j)
+{
+    if(_y == j.get_y())                          //Si au meme niveau
+    {
+        if(rand_a_b(0,10) <= 7) attaque();       //Attaque de manière aléatoire avec plus de probabilite
+        else defendre();
+
+    }else                                        //impossible attaquer
+    {
+        deplacer(j);
+    }
+
+}
+
+
+/* Detecte s'il y a un obstacle entre l'ennemi et le joueur
+* @Param: vect_x est la coordonnée x du vecteur joueur/ennemi
+* @Param: vect_y est la coordonnée y du vecteur joueur/ennemi
+* @Return: booléen 1 s'il y a des obstacles et 0 sinon.
+*/
+/*
+bool Ennemi::obstace(int vect_x, int vect_y, Map m)
+{
+
+}*/
+
+/* L'ennemi avance vers le joueur en fonction de la map
+* @Param: le joueur vers qui l'ennemi approche
+*/
+void Ennemi::deplacer(Joueur j)
+{
+    if(_y == j.get_y())                    //Meme niveau
+    {
+        if(_x > j.get_x()) _x = _x-1;      //prendre en compte peut être sur plateforme differente
+        else _x = _x+1;
+    }
+    else//if(_y >= j.get_y())                     //Ennemi au dessus personnage
+    {
+        _y = _y+1;                          //saut
+        if(_x > j.get_x()) _x = _x-1;
+        else _x = _x+1;
+    }
+}
+
+
+void Ennemi::defendre()
+{
+
+}
+
+void Ennemi::attaque()
+{
+
+}
+
+
 
 
 void Ennemi::affiche() const
