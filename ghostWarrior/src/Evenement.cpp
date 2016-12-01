4ca05ccedc5d09886fc4e87ed1148cdec7ad5cd8
SGD04 <sergiogalan-delea@hotmail.fr>
2016-12-01 09:09:35 +0100

    Update Evenement.cpp

--- ghostWarrior/src/Evenement.cpp
+++ ghostWarrior/src/Evenement.cpp
@@ -1,3 +1,25 @@
+/*
+ * Evenement.cpp
+ *
+ * Polytech Marseille
+ * Case 925 -163, avenue de Luminy
+ * 13288 Marseille CEDEX 9
+ *
+ * Ce fichier est l'oeuvre d'eleves de Polytech Marseille. Il ne peut être
+ * reproduit, utilise ou modifie sans l'avis express de ses auteurs.
+ */
+
+/**
+ * @author DENIS Justine <justine.denis@etu.univ-amu.fr>
+ * @author GALAN-DELEA Sergio <sergio.galan-delea@etu.univ-amu.fr>
+ *
+ * @version 1 / 01-12-2016
+ * @Resume: Classe des joueurs: personnages jouables par les utilisateurs
+ * @toto NA
+ * @bug NA
+*/
+
+
 #include "Evenement.hpp"
 
 /** \brief
