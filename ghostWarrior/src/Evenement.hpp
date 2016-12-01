ce0ecd8badd1e6c8290e4eed268a1c0afef27c92
SGD04 <sergiogalan-delea@hotmail.fr>
2016-12-01 09:00:31 +0100

    Create Evenement.hpp

---
+++ ghostWarrior/src/Evenement.hpp
@@ -0,0 +1,36 @@
+/*
+ * Evenement.hpp
+ *
+ * Polytech Marseille
+ * Case 925 -163, avenue de Luminy
+ * 13288 Marseille CEDEX 9
+ *
+ * Ce fichier est l'oeuvre d'eleves de Polytech Marseille. Il ne peut être
+ * reproduit, utilise ou modifie sans l'avis express de ses auteurs.
+ */
+/**
+ * @author DENIS Justine <justine.denis@etu.univ-amu.fr>
+ * @author GALAN-DELEA Sergio <sergio.galan-delea@etu.univ-amu.fr>
+ *
+ * @version 1 / 01-12-2016
+ * @Resume:
+ * @toto NA
+ * @bug NA
+*/
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Main
+{
+    public:
+    //attribut
+
+    //fonctions
+    Evenement();
+    ~Evenement();
+    void KeyBoardEventJ();
+};
