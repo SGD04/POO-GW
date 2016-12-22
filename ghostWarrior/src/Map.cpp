9ad3340693c4ce67e96130d9b9b6eaf17ee6644b
jsntju <jsnt.denis@gmail.com>
2016-12-22 16:16:25 +0100

    Nouvelle classe Map avec Vector comme tableau dynamique

--- ghostWarrior/src/Map.cpp
+++ ghostWarrior/src/Map.cpp
@@ -13,7 +13,7 @@
  * @author DENIS Justine <justine.denis@etu.univ-amu.fr>
  * @author GALAN-DELEA Sergio <sergio.galan-delea@etu.univ-amu.fr>
  *
- * @version 1 / 30-11-2016
+ * @version 2 / 30-11-2016
  * @Resume: Comprend l'architecture de l'environnement du jeu (background et plateforme)
  * @toto : Plus tard: faire la map avec les textures de SFML
  * @bug NA
@@ -25,103 +25,40 @@
  */
 Map::Map()
 {
+    using std::vector;
+
+    _grille.resize(NB_CASE_LONG);
+    for (int i = 0; i < NB_CASE_LAR; i++)               //Initialisation taille tableau dynamique
+    {
+      _grille[i].resize(NB_CASE_LONG);
+    }
+
     _background = 0;
     _plateforme = 1;
-    /*vector< vector<int> > _grille(LONGUEUR_ECRAN);
-    int h;
-    for(h = 0; h< LONGUEUR_ECRAN; h++)
+    _hautEcran = GetSystemMetrics( SM_CYSCREEN );
+    _longEcran = GetSystemMetrics( SM_CXSCREEN );
+    _largeurCase = _hautEcran/NB_CASE_LAR;       //taille écran /nb cases
+    _longueurCase =_longEcran/NB_CASE_LONG;      //taille écran /nb cases
+
+
+    /*Initialisation des données*/
+    int i,j;
+    for(i=0; i< _grille.size(); i++)
     {
-        _grille[h] = vector<int> (10);
-    }
-    for(int i=0; i < _grille.size(); i++)
-    {
-        for(int j=0; j < _grille[i].size(); j++)
+        for(j=0; j< _grille[i].size(); j++)
         {
             _grille[i][j] = 0;
         }
-    }*/
-   /* int i, j;
-      for(j=0; j<LONGUEUR_ECRAN; j++)
-    {
-        cout<<"J: "<<j<<"-> ";
-        for(i=0; i<LARGEUR_ECRAN; i++)
-        {
-            if(j%2 )
-            {
-                if( i == 0) _grille[j][i] == 1;
+    }
 
-                else if ((j== 1) && (i==1 || i==4)) _grille[j][i] == 1;
-                else if((j== 3) && ((i>=1 && i<=6) || (i == 8))) _grille[j][i] == 1;
-                else if((j== 5) && (i==3 || i==6 || i == 9)) _grille[j][i] == 1;
-                else if((j== 7) && (i==2 || i==4 || (i >= 6 && i<=9))) _grille[j][i] == 1;
-                else if((j==9)&& (i==1 || i==3 || i==4 || i==6 || i==7 || i==8)) _grille[j][i] == 1;
-                else _grille[j][i] == 0;
-            }
-            else  _grille[j][i] = 0;
-            cout<<_grille[j][i]<<"|";
-        }
-        cout<<endl;
-        cout<<"--------------------"<<endl;*/
-    int i, j;
-    for(j=0; j<LONGUEUR_ECRAN; j++)
+    for(i=0; i< _grille.size(); i++)
     {
-        for(i=0; i<LARGEUR_ECRAN; i++)
+        for(j=0; j< _grille[i].size(); j++)
         {
-            if(j%2 ) cout;
-            else _grille[j][i] = 0;
+           _grille[1][i] = 1;
         }
     }
-    _grille[1][0] = 1;
-    _grille[1][1] = 1;
-    _grille[1][2] = 0;
-    _grille[1][3] = 0;
-    _grille[1][4] = 1;
-    _grille[1][5] = 0;
-    _grille[1][6] = 0;
-    _grille[1][7] = 0;
-    _grille[1][8] = 0;
-    _grille[1][9] = 0;
-    _grille[3][0] = 1;
-    _grille[3][1] = 1;
-    _grille[3][2] = 1;
-    _grille[3][3] = 1;
-    _grille[3][4] = 1;
-    _grille[3][5] = 1;
-    _grille[3][6] = 1;
-    _grille[3][7] = 0;
-    _grille[3][8] = 1;
-    _grille[3][9] = 0;
-    _grille[5][0] = 1;
-    _grille[5][1] = 0;
-    _grille[5][2] = 0;
-    _grille[5][3] = 1;
-    _grille[5][4] = 0;
-    _grille[5][5] = 0;
-    _grille[5][6] = 1;
-    _grille[5][7] = 0;
-    _grille[5][8] = 0;
-    _grille[5][9] = 1;
-    _grille[7][0] = 1;
-    _grille[7][1] = 0;
-    _grille[7][2] = 1;
-    _grille[7][3] = 0;
-    _grille[7][4] = 1;
-    _grille[7][5] = 0;
-    _grille[7][6] = 1;
-    _grille[7][7] = 1;
-    _grille[7][8] = 1;
-    _grille[7][9] = 0;
-    _grille[9][0] = 1;
-    _grille[9][1] = 1;
-    _grille[9][2] = 0;
-    _grille[9][3] = 1;
-    _grille[9][4] = 1;
-    _grille[9][5] = 0;
-    _grille[9][6] = 1;
-    _grille[9][7] = 1;
-    _grille[9][8] = 1;
-    _grille[9][9] = 0;
-    _grille[4][4] = 1;
+
 }
 
 /** \brief Constructeur par recopie d'un objet MAP
@@ -130,27 +67,38 @@ Map::Map()
  */
 Map::Map(const Map & c)
 {
-    _grille[LONGUEUR_ECRAN][LARGEUR_ECRAN] = c._grille[LONGUEUR_ECRAN][LARGEUR_ECRAN];
-    for(int j=0; j<LONGUEUR_ECRAN; j++)
+    using std::vector;
+
+    int tailleI = c._grille[0].size();
+    _grille.resize(c._grille.size());
+    for (int i = 0; i < tailleI; i++)               //Initialisation taille tableau dynamique
     {
-        for(int i=0; i<LARGEUR_ECRAN; i++)
+      _grille[i].resize(c._grille.size());
+    }
+
+    int i ,j;                                       //copie tableau
+    for(i=0; i< _grille.size(); i++)
+    {
+        for(j=0; j< _grille[i].size(); j++)
         {
-            _grille[j][i] = c._grille[j][i];
+           _grille[i][j] = c._grille[i][j] ;
         }
     }
+
     _background = c._background;
     _plateforme = c._plateforme;
+    _longueurCase = c._longueurCase;
+    _largeurCase = c._largeurCase;
 }
 
 /** \brief Affichage en 0 et 1 de la map
  */
 void Map::afficher()
 {
-    cout<<"Grille :"<<endl;
-    for(int j=0; j<LONGUEUR_ECRAN; j++)
+    for(int j=0; j<_grille.size(); j++)
     {
         cout<<"J: "<<j<<"-> ";
-        for(int i=0; i<LARGEUR_ECRAN; i++)
+        for(int i=0; i<_grille[j].size(); i++)
         {
             cout<<_grille[j][i]<<"|";
         }
@@ -165,7 +113,19 @@ int Map::infos_case (int x, int y)
     return(_grille[y][x]);
 }
 
+
+float Map::getLongeurCase()
+{
+    return(_longueurCase);
+}
+
+float Map::getLargeurCase()
+{
+    return(_largeurCase);
+}
+
 Map::~Map()
 {
-    delete _grille;
+//    delete _grille;
 }
+
