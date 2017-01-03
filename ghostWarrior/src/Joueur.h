34e7c3ced5400686fa13a8422c1417edc9a52d7f
jsntju <jsnt.denis@gmail.com>
2017-01-03 13:08:04 +0100

    Update Joueur.h

--- ghostWarrior/src/Joueur.h
+++ ghostWarrior/src/Joueur.h
@@ -19,10 +19,15 @@
  * @bug NA
 */
 
+#include <math.h>
 #include <iostream>
 #include <string.h>
 using namespace std;
+#include "Bonus.h"
 #include "Personnage.h"
+#include "Evenement.hpp"
+
+
 
 #ifndef JOUEUR_H
 #define JOUEUR_H
@@ -33,16 +38,22 @@ class Joueur : public Personnage
     protected:
         string _nom;
 
+
     public:
-        Joueur(string nom, int n, int x, int y, int lo, int la);
+        Joueur(int n, int x, int y, float lo, float la, Map c, const string & nom);
+       // Joueur(int n, int x, int y, float lo, float la, Map c);
         virtual ~Joueur();
 
         void affiche() const;
         //void deplacer();
         int get_x();
         int get_y();
-
+        void colision(bool,bool,bool);
+        float get_longueur();
+        float get_largeur();
+        void changerAttribut(int);
 
 };
 
 #endif // JOUEUR_H
+
