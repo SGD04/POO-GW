c62026469b1c7fd2c591be6d15b8bca2edb5b8ed
jsntju <jsnt.denis@gmail.com>
2017-01-03 12:55:53 +0100

    Version avec defense

--- ghostWarrior/src/Personnage.h
+++ ghostWarrior/src/Personnage.h
@@ -13,7 +13,7 @@
  * @author DENIS Justine <justine.denis@etu.univ-amu.fr>
  * @author GALAN-DELEA Sergio <sergio.galan-delea@etu.univ-amu.fr>
  *
- * @version 1 / 24-11-2016
+ * @version 2 / 6-12-2016
  * @Resume: Classe mère des personnages du jeu.
  * @toto NA
  * @bug NA
@@ -24,7 +24,19 @@
 
 #include <SFML/Graphics.hpp>
 #include "Coordonne.h"
+#include "Map.h"
 #include <iostream>
+#include <time.h>
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+*/
+#include <Windows.h>                //pour creation des threds
+#include <tchar.h>
+#include <process.h>
+#include <stdio.h>
+//#include <strsafe.h>
 
 using namespace std;
 
@@ -34,16 +46,20 @@ class Personnage : public Coordonne
     protected:
         int _num;                        //Numero du personnage
         int _vie;                        //vie du personnage
+        Map _carte;                      // Carte du jeu
+        clock_t _lastDefense;              //derniere fois que le personnage a utilisé la defense
+        static unsigned int __stdcall faireDef(void* data);
+
 
 
     public:
-        Personnage(int n, int x, int y, int lo, int la);
-        Personnage(int x, int y, int lo, int la);
+        Personnage(int n, int x, int y, float lo, float la, Map c);
+        //Personnage(int x, int y, int lo, int la, Map c);
         virtual ~Personnage();
 
         void affiche(void) const;
          // player position
-        sf::Vector2f pos = sf::Vector2f(320, 240);
+
         // player velocity (per frame)
         sf::Vector2f vel = sf::Vector2f(0, 0);
         // gravity (per frame)
@@ -54,31 +70,32 @@ class Personnage : public Coordonne
         // run acceleration
         const float runacc = .15f;
         // max run velocity
-        const float maxrun = 0.5f;
+        const float maxrun = 0.3f;
         // jump acceleration
         const float jumpacc = -1;
         // number of frames to accelerate in
-        const unsigned char jumpframes = 13;
+        const unsigned int jumpframes = 15;
         // counts the number of frames where you can still accelerate
-        unsigned char jumpcounter = 0;
+        unsigned int jumpcounter = 0;
         //is perso on ground
         bool onground = false;
-        // inputs
-        bool left = false;
-        bool right = false;
-        bool jump = false;
+
+
 
         //fonctions
-        void Jump();
+        void Jump(bool jump);
         void Initval();
         bool BOnground();
         void ColisionEnv();
-        void UpdatePerso();
-        
+        void UpdatePerso(bool left, bool right);
+        void defense(bool def);
+
+       // virtual void colision();
+
        //deplacer();
       // virtual void defendre();
         //attaque();
-        //gravite();
+
 
 
 
@@ -86,3 +103,4 @@ class Personnage : public Coordonne
 };
 
 #endif // PERSONNAGE_H
+
